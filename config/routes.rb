11b20d49805424ec6188266cf8d284f8b97e6468
Britton Baumann <brittonbaumann@gmail.com>
2017-01-20 22:35:28 -0600

    added some style on navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'portfolio/portfolio'
+
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  get 'welcome/contact'
+
+  get 'welcome/blog'
+
+  root 'welcome#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
